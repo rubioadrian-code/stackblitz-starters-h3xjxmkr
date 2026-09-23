40d37e8ade8c233ba34a7c66eda218af91c3d814
rubioadrian-code <rubioadrian@gmail.com>
2026-09-23 15:58:39 -0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // Ignorar errores de formato en el despliegue
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // Ignorar avisos de tipado estricto en el despliegue
-      ignoreBuildErrors: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
